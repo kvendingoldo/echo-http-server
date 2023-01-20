9232bfd5ba7e3254131dc52b1e3f1419617a9157
Alexander Sharov <kvendingoldo@yandex.ru>
2023-01-20 01:51:10 +0100

    feat: add JSON logs support

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # echo-http-server
+
+A simple HTTP server is a program that listens for incoming requests from clients on a specified port (`SERVER_PORT`), 
+and then sends a response back to the client. The application is delivered as docker container.
+
+You may try it via the following command: `docker run -it -p 8089:8089 -e SERVER_PORT=8089 kvendingoldo/echo-http-server`
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"log"
+	"github.com/justinas/alice"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
 	"net/http"
 	"os"
+	"time"
 )
 
 const DefaultPort = "8080"
@@ -17,25 +20,44 @@ func getServerPort() string {
 	return DefaultPort
 }
 
-func EchoHandler(writer http.ResponseWriter, request *http.Request) {
-	log.Println("Echoing back request made to " + request.URL.Path + " to client (" + request.RemoteAddr + ")")
-
-	writer.Header().Set("Access-Control-Allow-Origin", "*")
-
-	writer.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
-
-	err := request.Write(writer)
-	if err != nil {
-		return
-	}
-}
-
 func main() {
-	log.Println("Starting server, listening on port " + getServerPort())
+	log := zerolog.New(os.Stdout).With().
+		Timestamp().
+		Logger()
 
-	http.HandleFunc("/", EchoHandler)
-	err := http.ListenAndServe(":"+getServerPort(), nil)
-	if err != nil {
-		return
+	c := alice.New()
+	c = c.Append(hlog.NewHandler(log))
+
+	// Install some provided extra handler to set some request's context fields.
+	// Thanks to that handler, all our logs will come with some prepopulated fields.
+	c = c.Append(hlog.AccessHandler(func(r *http.Request, status, size int, duration time.Duration) {
+		hlog.FromRequest(r).Info().
+			Str("method", r.Method).
+			Stringer("url", r.URL).
+			Int("status", status).
+			Int("size", size).
+			Dur("duration", duration).
+			Msg("")
+
+	}))
+	c = c.Append(hlog.RemoteAddrHandler("ip"))
+	c = c.Append(hlog.UserAgentHandler("user_agent"))
+	c = c.Append(hlog.RefererHandler("referer"))
+	c = c.Append(hlog.RequestIDHandler("req_id", "Request-Id"))
+
+	h := c.Then(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Range, Content-Disposition, Content-Type, ETag")
+
+		err := r.Write(w)
+		if err != nil {
+			return
+		}
+	}))
+
+	http.Handle("/", h)
+
+	if err := http.ListenAndServe(":"+getServerPort(), nil); err != nil {
+		log.Fatal().Err(err).Msg("Startup failed")
 	}
 }
